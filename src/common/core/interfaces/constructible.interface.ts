c6f2718088a400e12add5bae5ea72f4021aa4c97
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-06 16:47:29 +0300

    refactor: add definitions

--- src/common/core/interfaces/constructible.interface.ts
+++ src/common/core/interfaces/constructible.interface.ts
@@ -1,3 +1,7 @@
+/**
+ * A general-purpose constructible interface.
+ * @typeparam T The type of the object being constructed.
+ */
 export interface Constructible<T = any> {
   new (...args: any[]): T;
 }
