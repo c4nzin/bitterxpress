b3f717e37d62ef9864da7a45a00835563b8ebf45
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-06 21:11:26 +0300

    feat: add http method enum

---
+++ src/core/enums/http-methods.enum.ts
@@ -0,0 +1,11 @@
+export enum HttpMethod {
+  Get = 'get',
+  Head = 'head',
+  Post = 'post',
+  Put = 'put',
+  Delete = 'delete',
+  Connect = 'connect',
+  Options = 'options',
+  Trace = 'trace',
+  Patch = 'patch',
+}
