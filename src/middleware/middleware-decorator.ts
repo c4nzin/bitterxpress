9b1f61f989d716be578e7fada91cd8decf2af54c
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-06 16:43:53 +0300

    feat: add middleware decorator

---
+++ src/middleware/middleware-decorator.ts
@@ -0,0 +1,15 @@
+import { RequestHandler } from 'express';
+import 'reflect-metadata';
+import { Constructible } from '../common/core/interfaces/constructible.interface';
+
+const USE_MIDDLEWARES_METADATA = 'use:middlewares';
+
+export function UseMiddlewares(...middlewares: RequestHandler[]): ClassDecorator & MethodDecorator {
+  return function (target: Constructible | any, key?: string | symbol) {
+    if (key) {
+      Reflect.defineMetadata(USE_MIDDLEWARES_METADATA, middlewares, target, key);
+    } else {
+      Reflect.defineMetadata(USE_MIDDLEWARES_METADATA, middlewares, target);
+    }
+  };
+}
