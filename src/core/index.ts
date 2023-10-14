93cb5fc33b1e4818f7133a0936a0ff2b9a3eb4f3
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-14 14:12:39 +0300

    feat: add controller metadata enum

---
+++ src/core/index.ts
@@ -0,0 +1,7 @@
+export * from './common/middlewares/';
+export * from './decorators';
+export * from './decorators/http-status';
+export * from './enums/';
+export * from './http';
+export * from './interfaces';
+export * from './types';
---
+++ src/core/interfaces/controller-metadata.interface.ts
@@ -0,0 +1,9 @@
+import { RequestHandler } from 'express';
+import { Headers } from './headers.interface';
+
+export interface ControllerMetadata {
+  route: string;
+  middlewares?: RequestHandler[];
+  defaultHttpStatus?: number;
+  headers?: Headers;
+}
