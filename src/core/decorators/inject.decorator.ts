f87c2e90c545cee2368e08491505c60a51551872
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-14 15:12:26 +0300

    fix: miss-spelling keyword

--- src/core/decorators/inject.decorator.ts
+++ src/core/decorators/inject.decorator.ts
@@ -4,7 +4,7 @@ import { INJECT_TOKEN_METADATA } from '../enums';
 export function Inject(token: string): any {
   return function (target: any, key: string | symbol, index: number) {
     const injectTokens: Record<number, string> =
-      Reflect.getMetadata(, target, key) || {};
+      Reflect.getMetadata(INJECT_TOKEN_METADATA.INJECT_TOKEN, target, key) || {};
     injectTokens[index] = token;
     Reflect.defineMetadata(INJECT_TOKEN_METADATA.INJECT_TOKEN, injectTokens, target, key);
   };
