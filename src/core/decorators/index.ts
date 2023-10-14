aae0915c6f1c5e5fd543dc25b7366db2903551b1
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-14 14:11:40 +0300

    feat: add controller metadata key enum

---
+++ src/core/decorators/index.ts
@@ -0,0 +1,6 @@
+export * from './app.decorator';
+export * from './argument-decorator.factory';
+export * from './express-app-instance.decorator';
+export * from './inject.decorator';
+export * from './injectable.decorator';
+export * from './response-headers.decorator';
---
+++ src/core/enums/controller-metadata.enum.ts
@@ -0,0 +1,3 @@
+export enum ControllerMetadataKey {
+  ROUTE = 'route',
+}
