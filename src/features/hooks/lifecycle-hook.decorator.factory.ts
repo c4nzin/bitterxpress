6b946d1629f4f53fa8da263e6ea77f4016c935ab
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-14 15:23:45 +0300

    refactor: on hooks

--- src/features/hooks/lifecycle-hook.decorator.factory.ts
+++ src/features/hooks/lifecycle-hook.decorator.factory.ts
@@ -1,7 +1,5 @@
-export function lifecycleHookDecoratorFactory(metadataKey: string): Function {
-  return function (): MethodDecorator {
-    return function (target: any, key: string | symbol) {
-      Reflect.defineMetadata(metadataKey, key, target);
-    };
-  };
+export function lifecycleHookDecoratorFactory(metadataKey: string) {
+  return function (target: any, key: string | symbol) {
+    Reflect.defineMetadata(metadataKey, key, target);
+  } as MethodDecorator;
 }
