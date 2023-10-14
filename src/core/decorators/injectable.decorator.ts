3bdebcbc0069e243bbfea48599e02d1d9cdf3afd
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-14 13:20:51 +0300

    fix: injectable decorator

--- src/core/decorators/injectable.decorator.ts
+++ src/core/decorators/injectable.decorator.ts
@@ -1,11 +1,10 @@
+import { DependencyContainer } from '../../features/dependency.container';
 import { InjectableOptions } from '../interfaces/injectable-options.interface';
 
 export function Injectable(injectableOptions?: InjectableOptions): ClassDecorator {
   return function (target: any) {
     if (injectableOptions?.singleton === false) {
-      //   DependencyContainer.registerBlbalbla(target); not working
+      DependencyContainer.registerClassTokenDependency(target);
     }
   };
 }
-
-//Issue: Create Dependency Container
