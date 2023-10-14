b7ee5e0ac4e15c788d2312ab7add797704c101fe
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-14 13:22:23 +0300

    refactor: change function name

--- src/core/decorators/http-status/http-status.decorator.ts
+++ src/core/decorators/http-status/http-status.decorator.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 
 const DEFAULT_HTTP_STATUS_TOKEN = 'default-http-status';
 
-export function HttpStatus(code: number): ClassDecorator & MethodDecorator {
+export function DefaultHttpStatus(code: number): ClassDecorator & MethodDecorator {
   return function (target: any, key?: string | symbol) {
     if (key) Reflect.defineMetadata(DEFAULT_HTTP_STATUS_TOKEN, code, target, key);
     else Reflect.defineMetadata(DEFAULT_HTTP_STATUS_TOKEN, code, target);
