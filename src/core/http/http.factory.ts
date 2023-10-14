faa9e380999abd87efc699fb7220b593c217ac41
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-14 15:35:11 +0300

    refactor: use enums instead of using inline consts

--- src/core/http/http.factory.ts
+++ src/core/http/http.factory.ts
@@ -1,13 +1,11 @@
+import { HttpMethodMetadataKey } from '../enums';
 import { HttpMethod } from '../enums/http-methods.enum';
-
-export const METHOD_TOKEN = 'method';
-export const PATH_TOKEN = 'path';
 
 export function httpMethodDecoratorFactory(method: HttpMethod) {
   return function (path?: string): MethodDecorator {
     return function (target: any, key: string | symbol) {
-      Reflect.defineMetadata(METHOD_TOKEN, method, target, key);
-      Reflect.defineMetadata(PATH_TOKEN, path, target, key);
+      Reflect.defineMetadata(HttpMethodMetadataKey.METHOD, method, target, key);
+      Reflect.defineMetadata(HttpMethodMetadataKey.PATH, path, target, key);
     };
   };
 }
