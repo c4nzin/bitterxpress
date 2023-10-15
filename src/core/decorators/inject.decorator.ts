92a9ea241103020a11fa9217b0c2c56d203c4985
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-15 12:12:02 +0300

    Revert "refactor: remove record types"
    This reverts commit 88970b85f340bdb63f3021d12e9a2b8e48ca2757.

--- src/core/decorators/inject.decorator.ts
+++ src/core/decorators/inject.decorator.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata';
-import { DependencyInjectionMetadataKey } from '../enums';
+import { INJECT_TOKEN_METADATA } from '../enums';
 
 export function Inject(token: string): any {
   return function (target: any, key: string | symbol, index: number) {
-    const injectTokens: { [P: number]: string } =
-      Reflect.getMetadata(DependencyInjectionMetadataKey.INJECT_TOKENS, target, key) || {};
+    const injectTokens: Record<number, string> =
+      Reflect.getMetadata(INJECT_TOKEN_METADATA.INJECT_TOKEN, target, key) || {};
     injectTokens[index] = token;
-    Reflect.defineMetadata(DependencyInjectionMetadataKey.INJECT_TOKENS, injectTokens, target, key);
+    Reflect.defineMetadata(INJECT_TOKEN_METADATA.INJECT_TOKEN, injectTokens, target, key);
   };
 }
