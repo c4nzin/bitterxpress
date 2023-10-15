cce5163a395baf563fa642e2bf2e1b9e362cd919
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-15 11:05:25 +0300

    fix: unknown causes unexpected issues

--- src/core/interfaces/dependency.interface.ts
+++ src/core/interfaces/dependency.interface.ts
@@ -1,6 +1,6 @@
 import { Token } from '../types/token.type';
 
-export interface Dependency<T = unknown> {
+export interface Dependency<T = any> {
   token: Token<T>;
   instance?: T;
 }
--- src/features/dependency.container.ts
+++ src/features/dependency.container.ts
@@ -1,64 +1,58 @@
-import 'reflect-metadata';
-import { DependencyInjectionMetadataKey } from '../core/enums/injection-enums/depedency-injection-keys.enum';
-import { Constructible } from '../core/interfaces/constructible.interface';
-import { Token } from '../core/types/token.type';
+import { Dependency, DependencyInjectionMetadataKey, Constructible, Token } from '../core';
 
-class DependencyContainer {
-  private static readonly dependencies: Map<Token, any> = new Map();
+export class DependencyContainer {
+  private static readonly dependencies: Dependency[] = [];
 
-  public static get<T = any>(token: Token<T>): T {
-    if (!DependencyContainer.dependencies.has(token)) {
+  static get<T = any>(token: Token<T>): T {
+    let dependency: Dependency<T> | undefined = DependencyContainer.dependencies.find(
+      (dependency: Dependency) => dependency.token === token,
+    );
+    if (!dependency) {
       const instance: T = DependencyContainer.resolve(token);
-      DependencyContainer.dependencies.set(token, instance);
+      dependency = { token, instance };
+      DependencyContainer.dependencies.push(dependency);
     }
-
-    return DependencyContainer.dependencies.get(token);
+    if (dependency?.instance) {
+      return dependency.instance;
+    }
+    return DependencyContainer.resolve(token);
   }
 
-  public static resolve<T>(token: Token<T>): T {
-    if (typeof token === 'string') throw 'DependencyContainer: Unknown token identifier';
-
+  static resolve<T>(token: Token<T>): T {
+    if (typeof token === 'string') {
+      throw 'Unknown token identifier!';
+    }
     const constructorParamTypes: any[] = Reflect.getMetadata(
       DependencyInjectionMetadataKey.PARAMTYPES,
       token,
     );
-
     const injectTokens: Token[] = Reflect.getMetadata(
       DependencyInjectionMetadataKey.INJECT_TOKENS,
       token,
     );
-
-    if (!constructorParamTypes) {
-      return new token();
-    }
-
-    if (!constructorParamTypes.length) {
-      return new token();
-    }
-
-    const constructorParamInstances: any[] = constructorParamTypes.map((paramType, index) => {
-      const injectToken: Token = (injectTokens && injectTokens[index]) || paramType;
-
-      if (!injectToken) {
-        throw `DependencyContainer: Unable to resolve dependency of ${token} at index ${index}`;
+    const constructorParamInstances: any[] = [];
+    for (const i in constructorParamTypes) {
+      let injectToken: Token = constructorParamTypes[i];
+      if (injectTokens) {
+        injectToken = injectTokens[i] || injectToken;
       }
-
-      return DependencyContainer.get(injectToken);
-    });
-
+      if (!injectToken) {
+        throw `Cannot resolve dependency of ${token} at index ${i}`;
+      }
+      constructorParamInstances.push(DependencyContainer.get(injectToken));
+    }
     return new token(...constructorParamInstances);
   }
 
-  public static registerStringTokenDependency(token: string, instance: any) {
-    DependencyContainer.registerDependency(token, instance);
-  }
-  public static registerClassTokenDependency<T>(token: Constructible<T>, instance?: any) {
+  static registerStringTokenDependency(token: string, instance: any) {
     DependencyContainer.registerDependency(token, instance);
   }
 
-  public static registerDependency<T = any>(token: Constructible<T> | string, instance?: any) {
-    DependencyContainer.dependencies.set(token, instance);
+  static registerClassTokenDependency<T>(token: Constructible<T>, instance?: any) {
+    DependencyContainer.registerDependency(token, instance);
+  }
+
+  private static registerDependency<T = any>(token: Constructible<T> | string, instance?: any) {
+    DependencyContainer.dependencies.push({ token, instance });
   }
 }
-
-export { DependencyContainer };
