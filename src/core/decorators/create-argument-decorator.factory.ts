5162142fc3cc0c31628c814a758e7c03e2efd998
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-17 23:54:53 +0300

    fix: issue create argument decorator

--- src/core/decorators/create-argument-decorator.factory.ts
+++ src/core/decorators/create-argument-decorator.factory.ts
@@ -1,5 +1,7 @@
 export function createArgumentDecorator(metadataKey: string) {
   return (target: any, key: string | symbol, index: number) => {
-    Reflect.defineMetadata(metadataKey, index, target, key);
+    const existingIndexes = Reflect.getMetadata(metadataKey, target, key) || [];
+    existingIndexes.push(index);
+    Reflect.defineMetadata(metadataKey, existingIndexes, target, key);
   };
 }
