e25ebc483392ef65c11b0c733d3931c23d5a1610
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-17 22:34:23 +0300

    feat: add dependency container test

---
+++ src/core/container/dependency.container.test.ts
@@ -0,0 +1,59 @@
+import { DependencyContainer } from './dependency.container';
+import 'reflect-metadata';
+
+describe('DependencyContainer', () => {
+  afterEach(() => {
+    DependencyContainer['dependencies'].clear();
+  });
+
+  it('should register and retrieve a dependency', () => {
+    // Arrange
+    const exampleDependency = 'Example Dependency';
+
+    // Act
+    DependencyContainer.registerStringTokenDependency('exampleToken', exampleDependency);
+    const retrievedDependency = DependencyContainer.get('exampleToken');
+
+    // Assert
+    expect(retrievedDependency).toBe(exampleDependency);
+  });
+
+  it('should resolve a class dependency with no constructor parameters', () => {
+    // Arrange
+    class ExampleClass {}
+    DependencyContainer.registerClassTokenDependency(ExampleClass);
+
+    // Act
+    const resolvedInstance = DependencyContainer.resolve(ExampleClass);
+
+    // Assert
+    expect(resolvedInstance).toBeInstanceOf(ExampleClass);
+  });
+
+  it('should resolve a class dependency with constructor parameters', () => {
+    // Arrange
+    class DependencyA {}
+    class DependencyB {}
+
+    class ExampleClass {
+      constructor(a: DependencyA, b: DependencyB) {}
+    }
+
+    DependencyContainer.registerClassTokenDependency(DependencyA);
+    DependencyContainer.registerClassTokenDependency(DependencyB);
+    DependencyContainer.registerClassTokenDependency(ExampleClass);
+
+    // Act
+    const resolvedInstance = DependencyContainer.resolve(ExampleClass);
+
+    // Assert
+    expect(resolvedInstance).toBeInstanceOf(ExampleClass);
+  });
+
+  it('should throw an error when resolving an unknown token', () => {
+    // Act & Assert
+    expect(() => DependencyContainer.resolve('unknownToken')).toThrowError(
+      'DependencyContainer: Unknown token identifier',
+    );
+  });
+});
