e4acc34cfd3382194553b54d976a9034f2033695
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-10-17 23:31:07 +0300

    refactor: rename http status decorator

--- src/core/decorators/http-status/set-http-status.decorator.ts
+++ src/core/decorators/http-status/set-http-status.decorator.ts
@@ -1,6 +1,6 @@
 import { DEFAULT_HTTP_STATUS_TOKEN } from './http-status.constant';
 
-export function DefaultHttpStatus(code: number): ClassDecorator & MethodDecorator {
+export function SetDefaultHttpStatus(code: number): ClassDecorator & MethodDecorator {
   return function (target: any, key?: string | symbol) {
     if (key) Reflect.defineMetadata(DEFAULT_HTTP_STATUS_TOKEN, code, target, key);
     else Reflect.defineMetadata(DEFAULT_HTTP_STATUS_TOKEN, code, target);
